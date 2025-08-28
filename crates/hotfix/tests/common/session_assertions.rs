ac5b20d4b33f390be8c44693840dec3b88516da4
David Steiner <david_j_steiner@yahoo.co.nz>
2025-08-28 12:16:00 +0200

    Yet another increase to test timeouts

--- crates/hotfix/tests/common/session_assertions.rs
+++ crates/hotfix/tests/common/session_assertions.rs
@@ -2,7 +2,7 @@ use crate::common::test_messages::TestMessage;
 use hotfix::session::{SessionRef, Status};
 use std::time::Duration;
 
-pub const DEFAULT_TIMEOUT: Duration = Duration::from_millis(100);
+pub const DEFAULT_TIMEOUT: Duration = Duration::from_millis(500);
 
 pub trait SessionAssertions {
     async fn then_status_changes_to(&self, expected_status: Status);
