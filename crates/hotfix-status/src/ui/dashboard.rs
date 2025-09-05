923a9a3a97f631c076b42467977ba01d1a683666
David Steiner <david_j_steiner@yahoo.co.nz>
2025-09-05 14:43:17 +0200

    Improve dashboard style

--- crates/hotfix-status/src/ui/dashboard.rs
+++ crates/hotfix-status/src/ui/dashboard.rs
@@ -3,23 +3,28 @@ use crate::data_provider::DataProvider;
 use askama::Template;
 use axum::extract::State;
 use axum::response::{Html, IntoResponse};
+use chrono::Utc;
 use hotfix::session::SessionInfo;
+use hotfix::session::Status::{Active, Disconnected};
 
 #[derive(Template)]
 #[template(path = "dashboard.askama")]
 struct DashboardTemplate<'a> {
     title: &'a str,
     session_info: SessionInfo,
+    timestamp_string: &'a str,
 }
 
 pub(crate) async fn dashboard_handler<P: DataProvider>(
     State(state): State<AppState<P>>,
 ) -> impl IntoResponse {
     let session_info = state.data_provider.get_session_info().await;
+    let timestamp_string = Utc::now().to_rfc3339();
 
     let template = DashboardTemplate {
         title: "Dashboard",
         session_info,
+        timestamp_string: &timestamp_string,
     };
     Html(template.render().unwrap())
 }
