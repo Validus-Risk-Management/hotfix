15391ea04157ae50fd7494e44615d44026d7236e
David Steiner <david_j_steiner@yahoo.co.nz>
2025-09-05 13:37:50 +0200

    Make UI functionality properly dependent on UI feature flag

--- crates/hotfix-status/src/lib.rs
+++ crates/hotfix-status/src/lib.rs
@@ -1,9 +1,9 @@
 mod api;
 mod data_provider;
+#[cfg(feature = "ui")]
 mod ui;
 
 use crate::api::build_api_router;
-use crate::ui::builder_ui_router;
 use axum::Router;
 use hotfix::message::FixMessage;
 use hotfix::session::SessionRef;
@@ -13,8 +13,14 @@ struct AppState<P> {
     data_provider: P,
 }
 
+#[cfg(feature = "ui")]
 pub fn build_router<M: FixMessage>(session_ref: SessionRef<M>) -> Router {
     Router::new()
         .nest("/api", build_api_router(session_ref))
-        .merge(builder_ui_router())
+        .merge(crate::ui::builder_ui_router())
+}
+
+#[cfg(not(feature = "ui"))]
+pub fn build_router<M: FixMessage>(session_ref: SessionRef<M>) -> Router {
+    Router::new().nest("/api", build_api_router(session_ref))
 }
