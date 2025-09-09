af2fb18ebf0d3f1f758065995970b91b1b6e9f77
David Steiner <david_j_steiner@yahoo.co.nz>
2025-09-09 11:26:22 +0200

    Add test cases for UI views of hotfix-status

--- crates/hotfix-status/src/lib.rs
+++ crates/hotfix-status/src/lib.rs
@@ -60,17 +60,17 @@ mod tests {
         }
     }
 
+    const DATA_PROVIDER: &FakeDataProvider = &FakeDataProvider {
+        session_info: SessionInfo {
+            next_sender_seq_number: 3,
+            next_target_seq_number: 5,
+            status: Status::AwaitingLogon,
+        },
+    };
+
     #[tokio::test]
     async fn test_get_health() {
-        let data_provider = FakeDataProvider {
-            session_info: SessionInfo {
-                next_sender_seq_number: 3,
-                next_target_seq_number: 5,
-                status: Status::AwaitingLogon,
-            },
-        };
-
-        let mut router = build_router_with_provider(data_provider);
+        let mut router = build_router_with_provider(DATA_PROVIDER.clone());
 
         let response = router
             .call(Request::get("/api/health").body::<Body>("".into()).unwrap())
@@ -82,15 +82,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_session_info() {
-        let data_provider = FakeDataProvider {
-            session_info: SessionInfo {
-                next_sender_seq_number: 3,
-                next_target_seq_number: 5,
-                status: Status::AwaitingLogon,
-            },
-        };
-
-        let mut router = build_router_with_provider(data_provider);
+        let mut router = build_router_with_provider(DATA_PROVIDER.clone());
 
         let response = router
             .call(
@@ -126,5 +118,59 @@ mod tests {
             .and_then(|status| status.as_str())
             .unwrap();
         assert_eq!("AwaitingLogon", status);
+    }
+
+    #[cfg(feature = "ui")]
+    #[tokio::test]
+    async fn test_get_dashboard() {
+        let mut router = build_router_with_provider(DATA_PROVIDER.clone());
+        let response = router
+            .call(Request::get("/").body::<Body>("".into()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(200, response.status());
+
+        let headers = response.headers();
+        assert_eq!(
+            headers.get("content-type").unwrap(),
+            "text/html; charset=utf-8"
+        );
+    }
+
+    #[cfg(not(feature = "ui"))]
+    #[tokio::test]
+    async fn test_get_dashboard_without_ui_feature_returns_404() {
+        let mut router = build_router_with_provider(DATA_PROVIDER.clone());
+        let response = router
+            .call(Request::get("/").body::<Body>("".into()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(404, response.status());
+    }
+
+    #[cfg(feature = "ui")]
+    #[tokio::test]
+    async fn test_get_static_assets() {
+        let mut router = build_router_with_provider(DATA_PROVIDER.clone());
+        let response = router
+            .call(
+                Request::get("/static/tailwind.js")
+                    .body::<Body>("".into())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(200, response.status());
+
+        let headers = response.headers();
+        let content_type = headers.get("content-type").unwrap().to_str().unwrap();
+        assert!(
+            content_type.contains("application/javascript")
+                || content_type.contains("text/javascript")
+                || content_type.contains("application/x-javascript")
+        );
     }
 }
