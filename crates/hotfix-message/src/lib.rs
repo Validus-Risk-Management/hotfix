ed1ac93c34ff249bdc2715df63f0f633d96d9b00
David Steiner <david_j_steiner@yahoo.co.nz>
2025-10-31 13:04:24 +0100

    Expose FieldValueError publicly

--- crates/hotfix-message/src/lib.rs
+++ crates/hotfix-message/src/lib.rs
@@ -9,12 +9,12 @@ pub(crate) mod parts;
 mod tags;
 
 pub use encoding::Buffer;
-pub use encoding::HardCodedFixFieldDefinition;
 pub use encoding::field_access::FieldType;
 pub use encoding::field_types;
 #[cfg(feature = "fix42")]
 pub use encoding::fix42;
 pub use encoding::fix44;
+pub use encoding::{FieldValueError, HardCodedFixFieldDefinition};
 pub use hotfix_derive::FieldType;
 pub use hotfix_dictionary::{self as dict, TagU32};
 pub use parser::SOH;
