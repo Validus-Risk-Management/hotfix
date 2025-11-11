f64738e5b0bfb3df8d6a180505145fb957a71205
David Steiner <david_j_steiner@yahoo.co.nz>
2025-11-11 13:35:44 +0100

    Add test case for sending heartbeats in response to test requests

--- crates/hotfix/tests/session_test_cases/heartbeat_tests.rs
+++ crates/hotfix/tests/session_test_cases/heartbeat_tests.rs
@@ -1,7 +1,9 @@
 use crate::common::actions::when;
 use crate::common::assertions::{assert_msg_type, then};
 use crate::common::setup::{HEARTBEAT_INTERVAL, given_an_active_session};
-use hotfix_message::fix44::MsgType;
+use hotfix::message::test_request::TestRequest;
+use hotfix_message::Part;
+use hotfix_message::fix44::{MsgType, TEST_REQ_ID};
 use std::time::Duration;
 
 /// Tests the automatic heartbeat mechanism in an active FIX session:
@@ -62,5 +64,27 @@ async fn test_peer_timeout() {
 
     // we wait even longer and the counterparty never responds, so we disconnect from the counterparty
     when(Duration::from_secs(peer_interval)).elapses().await;
+    then(&mut mock_counterparty).gets_disconnected().await;
+}
+
+/// Tests that we send a heartbeat in response to Test Requests.
+///
+/// The `TestReqID` of the heartbeat (field 112) should match that of the request.
+#[tokio::test(start_paused = true)]
+async fn test_heartbeat_in_response_to_test_request() {
+    let (session, mut mock_counterparty) = given_an_active_session().await;
+
+    let test_request = TestRequest::new("abc-123".to_string());
+    when(&mut mock_counterparty)
+        .sends_message(test_request)
+        .await;
+    then(&mut mock_counterparty)
+        .receives(|msg| {
+            assert_msg_type(msg, MsgType::Heartbeat);
+            assert_eq!(msg.get::<&str>(TEST_REQ_ID).unwrap(), "abc-123");
+        })
+        .await;
+
+    when(&session).requests_disconnect().await;
     then(&mut mock_counterparty).gets_disconnected().await;
 }
