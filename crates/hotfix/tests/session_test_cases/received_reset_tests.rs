d55199fd8f9c004aaedcbe8368bf02dfc06d206e
David Steiner <david_j_steiner@yahoo.co.nz>
2025-12-08 14:12:34 +0100

    Add test case for processing sequence reset with sequence number lower than expected

--- crates/hotfix/tests/session_test_cases/received_reset_tests.rs
+++ crates/hotfix/tests/session_test_cases/received_reset_tests.rs
@@ -11,6 +11,7 @@ use crate::common::actions::when;
 use crate::common::assertions::then;
 use crate::common::cleanup::finally;
 use crate::common::setup::given_an_active_session;
+use crate::common::test_messages::TestMessage;
 
 /// Tests that the session correctly processes an inbound SequenceReset message
 /// with `NewSeqNo` higher than the current target sequence number.
@@ -26,6 +27,38 @@ async fn test_receive_reset_with_new_seq_number_higher_than_current() {
         .await;
     then(&mut session)
         .target_sequence_number_reaches(NEW_SEQ_NO - 1)
+        .await;
+
+    finally(&session, &mut counterparty).disconnect().await;
+}
+
+/// Tests that the reset is processed even when the sequence number is off.
+///
+/// For example, a sequence number too low would normally result in a termination
+/// of the session, but for non-gap fill resets, it is happily accepted.
+#[test]
+async fn test_sequence_number_is_ignored_in_resets() {
+    let (mut session, mut counterparty) = given_an_active_session().await;
+
+    // the counterparty sends a message, but due to a failure, it's lost
+    let sequence_number = counterparty.next_target_sequence_number();
+    when(&mut counterparty)
+        .sends_message(TestMessage::dummy_execution_report())
+        .await;
+    then(&mut session)
+        .target_sequence_number_reaches(sequence_number)
+        .await;
+    counterparty.delete_last_message_from_store();
+
+    // the counterparty sends a reset with the sequence number being the same
+    // as the previous message's
+    // the new sequence number is higher than what we currently have
+    let new_sequence_number = sequence_number + 10;
+    when(&mut counterparty)
+        .sends_sequence_reset(sequence_number, new_sequence_number)
+        .await;
+    then(&mut session)
+        .target_sequence_number_reaches(new_sequence_number - 1)
         .await;
 
     finally(&session, &mut counterparty).disconnect().await;
